dbef3b2cd7953cb5b9265b32af97f23c79e61630
Adam McCarthy <adam.mccarthy@taktile.com>
2026-01-06 23:09:24 +0100

    feature: posthog

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { PostHogProvider } from 'posthog-js/react';
 
-import App from "./App";
+import App from './App';
 
-const rootElement = document.getElementById("root");
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-11-30',
+} as const;
+
+const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+root.render(
+  <PostHogProvider
+    apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+    options={options}
+  >
+    <App />
+  </PostHogProvider>
+);
